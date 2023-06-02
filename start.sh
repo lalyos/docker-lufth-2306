e642887dfe0a7abfbb9c222cb11edadb12cac59b
lalyos <lalyos@yahoo.com>
2023-06-02 14:50:37 +0200

    introduce COLOR and BODY

--- start.sh
+++ start.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
 
-echo afternoon coffee break > /var/www/html/index.html
+cat > /var/www/html/index.html <<EOF
+<html lang="en">
+<head>
+    <title>Document</title>
+</head>
+<body bgcolor="${COLOR:-gray}" >
+    <h1>${TITLE:-Welcome}</h1>
+    ${BODY:- Please use TITLE/COLOR/BODY env vars}
+</body>
+</html>
+EOF
 
 nginx -g "daemon off;"
+# last line ...
