b2cfe69ee36c4772def0edbdbf1bb4c37e323016
lalyos <lalyos@yahoo.com>
2023-06-09 12:39:41 +0200

    add sql

---
+++ sql/init.sql
@@ -0,0 +1,6 @@
+use mysql;
+create table vip (name varchar(20), age int);
+insert into vip values ('Elon', 47);
+insert into vip values ('Bill', 58);
+insert into vip values ('Layos', 49);
+# one more line ...
